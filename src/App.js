a4ebd0d88b30d91380bc5705fe16fe879edacd25
evanmartiin <evan.martiin29@gmail.com>
2020-11-09 17:39:34 +0100

    le debug de malade

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css'
-import SpotifyWrapper from 'components/SpotifyWrapper';
 import Home from 'pages/Home';
 
 function App() {
   return (
-    <SpotifyWrapper>
-      <Home />
-    </SpotifyWrapper>
+    <Home />
   );
 }
 
