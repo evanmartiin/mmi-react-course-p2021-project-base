8d60a9f467fd2c6c58867488471c13be7ceec7ef
evanmartiin <evan.martiin29@gmail.com>
2020-11-09 15:11:48 +0100

    test

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Home from 'pages/Home';
 
 function App() {
   return (
-    <SpotifyWrapper>
+    /*<SpotifyWrapper>
       <Home />
-    </SpotifyWrapper>
+    </SpotifyWrapper>*/
+    Test
   );
 }
 
