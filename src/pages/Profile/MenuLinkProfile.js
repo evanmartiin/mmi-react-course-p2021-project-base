320253aadbd90500a35a65a7ec3db801125ff245
Juliejqm <juliejoaquim10@gmail.com>
2020-11-10 17:59:47 +0100

    Menu Profile avec premiers css

--- src/pages/Profile/MenuLinkProfile.js
+++ src/pages/Profile/MenuLinkProfile.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
+import styles from './MenuProfile.module.css'
+
 const MenuLinkProfile = (props) => {
     
     return (
         <div>
-            <Link to={props.to}>
+            <Link to={props.to} className={styles.menuProfileItem}>
             {props.children}
             </Link>
         </div>
