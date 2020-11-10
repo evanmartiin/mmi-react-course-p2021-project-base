2dd73071fb23a416ee0003a81c4e6c50a2dfec95
justinqllvr <justin.quillevere@outlook.fr>
2020-11-10 16:19:43 +0100

    Questionnaire

---
+++ src/pages/Form/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import FormInput from './FormInput'
+
+const Form = () => {
+    return (
+        <div>
+            <FormInput>
+                BLABLA 1
+            </FormInput>
+            <FormInput>
+                BLABLA 2
+            </FormInput>
+            <FormInput>
+                BLABLA 3
+            </FormInput>
+        </div>
+    )
+}
+
+export default Form
