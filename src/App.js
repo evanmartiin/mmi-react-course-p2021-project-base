437c0191226dca17908a8ad8d967dcc34923fa85
evanmartiin <evan.martiin29@gmail.com>
2020-11-10 12:51:55 +0100

    debug menu

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ function App() {
         <Route path="/profile">
           <Profile />
         </Route>
+        <Route path="/">
+          <Home />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
