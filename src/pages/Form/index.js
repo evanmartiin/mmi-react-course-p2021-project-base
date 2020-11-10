9a72d81fd50a29b6e35ad16a5c4865f215077ae8
justinqllvr <justin.quillevere@outlook.fr>
2020-11-10 18:02:17 +0100

    Questionnaire + Hook

--- src/pages/Form/index.js
+++ src/pages/Form/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import FormInput from './FormInput'
+import Layout from '../../components/Layout/Layout'
 
 const Form = () => {
     return (
-        <div>
-            <FormInput>
-                BLABLA 1
-            </FormInput>
-            <FormInput>
-                BLABLA 2
-            </FormInput>
-            <FormInput>
-                BLABLA 3
-            </FormInput>
-        </div>
+        <Layout removeMenu>
+            <div>
+                <h1> qu'est-ce qui te ferait plaisir ?</h1>
+                <FormInput>
+                    BLABLA 1
+                </FormInput>
+                <FormInput>
+                    BLABLA 2
+                </FormInput>
+                <FormInput>
+                    BLABLA 3
+                </FormInput>
+            </div>
+        </Layout>
     )
 }
 
