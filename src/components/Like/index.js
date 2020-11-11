26726ce70c6f496dea18624c46e5179eab4d8fee
evanmartiin <evan.martiin29@gmail.com>
2020-11-11 17:48:26 +0100

    pull

---
+++ src/components/Like/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import likedIcon from '../../assets/images/like-full.png'
+import unlikedIcon from '../../assets/images/like-empty.png'
+
+import './styles.css'
+
+const Like = (props) => {
+    const { liked } = props;
+
+    const [isLiked, updateisLiked] = useState(false)
+
+    const toggleLiked = () => {
+        updateisLiked(!isLiked)
+    }
+
+    return (
+        <button className="like" onClick={toggleLiked}>
+            <img src={isLiked ? likedIcon : unlikedIcon} alt="Aimer"/>
+        </button>
+    )
+}
+
+Like.defaultProps = {
+    liked: false
+}
+
+Like.propTypes = {
+    liked: PropTypes.bool.isRequired
+}
+
+export default Like
---
+++ src/components/Track/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Like from '../Like/index'
+import TrackSettings from '../TrackSettings/index'
+import listening from '../../assets/images/music-note.png'
+
+import './styles.css'
+
+const Track = (props) => {
+    const { artist, title, theme, isListening } = props;
+    return (
+        <div className="track">
+            <div className="trackCover">
+                {isListening === true &&
+                    <img className="listeningIcon" src={listening} alt="En écoute"/>
+                }
+                <img className="cover" src="https://static2.greatsong.net/artiste/276x276/foushee-99991200008421.jpg" alt="Cover"/>
+            </div>
+            <div className="trackInfos">
+                <p className="artist">{artist}</p>
+                <p className="title">{title}</p>
+                <p className="theme">{theme}</p>
+            </div>
+            <div className="trackSettings">
+                <Like />
+                <TrackSettings />
+            </div>
+        </div>
+    )
+}
+
+Track.defaultProps = {
+    isListening: false
+}
+
+Track.propTypes = {
+    isListening: PropTypes.bool.isRequired
+}
+
+export default Track
