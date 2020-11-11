41d56a283d6aa3f59d61f39e68fed42f2d5317c5
evanmartiin <evan.martiin29@gmail.com>
2020-11-11 14:04:54 +0100

    pull

--- src/pages/Form/FormInput.js
+++ src/pages/Form/FormInput.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import styles from './FormInput.module.css'
 
+import whiteMoon from '../../assets/images/moon-white.png'
+import blueMoon from '../../assets/images/moon-blue.png'
+
 const FormInput = (props) => {
     const {children} = props
 
@@ -13,6 +16,7 @@ const FormInput = (props) => {
         <div>
             <button type="button" className={isLiked ? styles.isLikedTrue : styles.isLikedFalse} onClick={toggleLike}>
                 {children}
+                <img src={isLiked ? blueMoon : whiteMoon} alt="Aimer"/>
             </button>
         </div>
     )
