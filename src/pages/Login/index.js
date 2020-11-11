35840df6421e3c404960c0812967eb8272a2dede
evanmartiin <evan.martiin29@gmail.com>
2020-11-11 15:27:32 +0100

    pull

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import Input from '../../components/Input/index'
+import Button from '../../components/Button'
 
 import './styles.css'
 
@@ -12,8 +13,11 @@ const Login = () => {
                     <div className="marginInput">
                         <Input type="email" placeholder="Mail" label="Adresse mail :"/>
                     </div>
-                    <Input type="password" placeholder="Mot de passe" label="Mot de passe :"/>
-                    <p className="forgetPass">Mot de passe oublié</p>
+                    <div>
+                        <Input type="password" placeholder="Mot de passe" label="Mot de passe :"/>
+                        <p className="forgetPass">Mot de passe oublié</p>
+                    </div>
+                    <Button linkTo="/Home">Connecte-toi</Button>
                 </div>
             </Layout>
         </div>
