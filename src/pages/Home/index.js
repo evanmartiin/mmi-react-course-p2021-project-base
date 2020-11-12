fc0a6cb7dc1484df8733bbb691e6b965b7a52e3a
justinqllvr <justin.quillevere@outlook.fr>
2020-11-12 17:50:49 +0100

    Acceuil style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,21 +2,32 @@ import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import { Link } from 'react-router-dom'
 import MusicType from '../../components/MusicType'
+import './styles.css'
+import ASMRType from '../../assets/images/ASMR.png'
+import respiration from '../../assets/images/respiration.png'
+import meditation from '../../assets/images/meditation.png'
+import ambiance from '../../assets/images/ambiance.png'
+
 
 const Home = () => {
     return (
         <div className="home">
             <Layout removeReturn>
-                Home
+                <div className="preferences">
+                    <div className="preferencesTop">
+                        <MusicType linkTo="/form"  imageType={ASMRType} title="ASMR"/>
+                        <MusicType linkTo="/form"  imageType={meditation} title="Méditation Hypnose"/>
+                    </div>
+                    <div className="preferencesBottom">
+                        <MusicType linkTo="/form"  imageType={respiration} title="Exercices de respiration"/>
+                        <MusicType linkTo="/form"  imageType={ambiance} title="Ambiance naturelle"/>
+                    </div>
+                </div>
                 <br/><Link to="/login">Login</Link>
                 <br/><Link to="/signup">Signup</Link>
                 <br/><Link to="/form">Form</Link>
                 <br/><Link to="/entrance">Entrance</Link>
-                <br/><Link to="/artist">Artist</Link>
-                <MusicType linkTo="/form" musicType="ASMR"/>
-                <MusicType linkTo="/form" musicType="ASMR"/>
-                <br/> 
-                <MusicType linkTo="/form" musicType="ASMR"/>
+                <br/><Link to="/artist">Artist</Link> 
             </Layout>
         </div>
     );
