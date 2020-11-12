f62afb34ee27266978aea42bdf773ef20acb68c0
evanmartiin <evan.martiin29@gmail.com>
2020-11-12 21:46:03 +0100

    pull

--- src/components/LittlePlayer/index.js
+++ src/components/LittlePlayer/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import './styles.css'
 import BackIcon from '../../assets/images/playerback-grey.png'
@@ -9,23 +9,26 @@ import NextIcon from '../../assets/images/playernext-grey.png'
 
 const LittlePlayer = () => {
     const [isPaused, updateisPaused] = useState(false)
-
     const togglePaused = () => {
         updateisPaused(!isPaused)
     }
 
+    const [isOpened, updateisOpened] = useState(false)
+    const toggleOpened= () => {
+        updateisOpened(!isOpened)
+        console.log(isOpened)
+    }
+
     return (
-        <div className="littlePlayer">
-            <Link to="/player">
+        <div className={isOpened ? `bigPlayer`: `littlePlayer`}>
+            <button className="toggleOpenedButton" onClick={toggleOpened}>
                 <img className="playerCover" src="https://images.genius.com/67f4a03a0afd422150b0a88fd94fea0b.300x300x1.png" alt="Cover" />
-            </Link>
-            <Link to="/player">
                 <div className="playerInfos">
                     <p className="artist">Klingande</p>
                     <p className="title">Alive</p>
                     <p className="theme">Classique</p>
                 </div>
-            </Link>
+            </button>
             <div className="playerSettings">
                 <img className="otherButtons" src={BackIcon} alt="Précédent" />
                 <button onClick={togglePaused}><img className="playButton" src={isPaused ? PauseIcon : PlayIcon} alt="Jouer" /></button>
@@ -33,6 +36,14 @@ const LittlePlayer = () => {
             </div>
         </div>
     )
+}
+
+LittlePlayer.defaultProps = {
+    isOpened: false
+}
+
+LittlePlayer.propTypes = {
+    isOpened: PropTypes.bool.isRequired
 }
 
 export default LittlePlayer
--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 
+import titleIcon from '../../assets/images/settings-white.png'
+import PageTitle from '../../components/PageTitle'
+
 const Settings = () => {
     return (
         <div>
             <Layout backTo="/profile">
-                Settings
+                <PageTitle icon={titleIcon}>Paramètres</PageTitle>
             </Layout>
         </div>
     );
