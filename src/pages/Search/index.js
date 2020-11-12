ff3adf092b432b1a6be7dc1bde486548c62305ef
justinqllvr <justin.quillevere@outlook.fr>
2020-11-12 17:58:57 +0100

    animation update

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
-import Tracks from './Tracks'
 
 const Search = () => {
     return (
         <div>
             <Layout removeReturn>
                 Search
-                <Tracks />
             </Layout>
         </div>
     );
