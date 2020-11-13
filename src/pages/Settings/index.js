ee993c2754ba882a8d72368e29f87c603caf9eb0
evanmartiin <evan.martiin29@gmail.com>
2020-11-13 11:17:51 +0100

    debug

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -3,14 +3,14 @@ import Layout from '../../components/Layout/Layout'
 
 import titleIcon from '../../assets/images/settings-white.png'
 import PageTitle from '../../components/PageTitle'
-import Tracks from './Tracks'
+// import Tracks from './Tracks'
 
 const Settings = () => {
     return (
         <div>
             <Layout backTo="/profile">
                 <PageTitle icon={titleIcon}>Paramètres</PageTitle>
-                <Tracks />
+                {/* <Tracks /> */}
             </Layout>
         </div>
     );
