1a7a7afb42830431ae0c6e4e2d03189e90cb578d
evanmartiin <evan.martiin29@gmail.com>
2020-11-13 14:02:26 +0100

    debug

--- src/components/TrackSettingsWindow/index.js
+++ src/components/TrackSettingsWindow/index.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import styles from './styles.module.css'
 
 const TrackSettingsWindow = (props) => {
