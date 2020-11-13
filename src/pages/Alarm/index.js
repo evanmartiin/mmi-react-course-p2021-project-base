d02612d995fed1642eb446519422b9bc23514b0b
justinqllvr <justin.quillevere@outlook.fr>
2020-11-13 12:37:31 +0100

    Start param + Alarm + Timer

--- src/pages/Alarm/index.js
+++ src/pages/Alarm/index.js
@@ -3,12 +3,15 @@ import Layout from '../../components/Layout/Layout'
 import PageTitle from '../../components/PageTitle/index'
 
 import titleIcon from '../../assets/images/reveil-white.png'
+import PageSubTitle from '../../components/PageSubTitle'
 
 const Alarm = () => {
     return (
         <div>
             <Layout backTo="/profile">
                 <PageTitle icon={titleIcon}>Réveil</PageTitle>
+                <PageSubTitle icon={titleIcon}>Paramétrer un réveil</PageSubTitle>
+                <PageSubTitle icon={titleIcon}>Tes réveils</PageSubTitle>
             </Layout>
         </div>
     );
